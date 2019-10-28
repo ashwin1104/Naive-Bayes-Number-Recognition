a6015fa29ea28a179f317adbf2ac879bc0fdaf4c
AlexMackowiak <Alex@Mackowiaks.com>
2019-10-28 11:17:42 -0500

    Initial commit

---
+++ libbayes/src/model.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "image.hpp"
+
+/*
+ * We've given you a starter struct to represent the model.
+ * You are totally allowed to delete, change, move, rename, etc. this struct
+ * however you like! In fact, we encourage it! It only exists as a starting
+ * point of reference.
+ *
+ * In our probabilities array we have a final dimension [2], which represents
+ * the individual probabilities that a pixel for a class is either shaded or
+ * not shaded. Since the probability that a pixel is shaded is just
+ * (1 - probability not shaded), we COULD have deleted that final dimension
+ * (and you can do so if you want to), but we left it in so that you could
+ * see how the model would need to change if we were to keep track of the
+ * probability that a pixel is white vs. gray vs. dark gray vs. black.
+ *
+ * You can delete this comment once you're done with it.
+ */
+
+constexpr size_t NUM_CLASSES = 10;  // 0-9 inclusive
+
+/**
+ * Represents a Naive Bayes classification model for determining the
+ * likelihood that an individual pixel for an individual class is
+ * white or black.
+ */
+struct Model {
+    // The individual probabilities for each pixel for each class for
+    // whether it's shaded or not.
+    //
+    // Examples:
+    // probs[2][10][7][0] is the computed probability that a pixel at
+    // [2][10] for class 7 is not shaded.
+    //
+    // probs[0][0][0][1] is the computed probability that a pixel at
+    // [0][0] for class 0 is shaded.
+    double probs[IMAGE_SIZE][IMAGE_SIZE][NUM_CLASSES][2];
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <image.hpp>
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return EXIT_SUCCESS;
+}
